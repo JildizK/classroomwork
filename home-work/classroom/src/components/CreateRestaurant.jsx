e30671f881c80937e3ecba92f4667c6f50a979c7
JildizK <jildizk13@gmail.com>
2023-08-24 11:21:43 +0600

    ya zdelal create

--- home-work/classroom/src/components/CreateRestaurant.jsx
+++ home-work/classroom/src/components/CreateRestaurant.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-
-const CreateRestaurant = () => {
-  return (
-    <div>CreateRestaurant</div>
-  )
-}
-
-export default CreateRestaurant
+import React, { useState, useEffect } from "react"; 
+import axios from "axios"; 
+import { useDispatch, useSelector } from "react-redux"; 
+import { createRestaurant } from "../store/restaurantsSlice"; 
+import { useNavigate } from "react-router-dom"; 
+ 
+import Box from "@mui/material/Box"; 
+import TextField from "@mui/material/TextField"; 
+import Stack from "@mui/material/Stack"; 
+import Button from "@mui/material/Button"; 
+ 
+const CreateRestaurant = () => { 
+    const dispatch = useDispatch(); 
+    const navigate = useNavigate(); 
+ 
+    const [restaurant, setRestaurant] = useState({ 
+        name: "", 
+        address: "", 
+        url: "", 
+        rating: 0, 
+        booking:[]
+    }); 
+ 
+    return ( 
+        <> 
+            <TextField onChange={(e) => setRestaurant({...restaurant, name:e.target.value})} id="standard-basic" label="Name" variant="standard" /> 
+            <TextField onChange={(e) => setRestaurant({...restaurant, address:e.target.value})} id="standard-basic" label="Image" variant="standard" /> 
+            <TextField onChange={(e) => setRestaurant({...restaurant, url:e.target.value})} id="standard-basic" label="Address" variant="standard" /> 
+ 
+            <Stack spacing={2} direction="row"> 
+                <Button variant="outlined" onClick={() => dispatch(createRestaurant(restaurant))}>Create</Button> 
+            </Stack> 
+        </> 
+    ); 
+}; 
+ 
+export default CreateRestaurant;
--- home-work/classroom/src/store/restaurantsSlice.js
+++ home-work/classroom/src/store/restaurantsSlice.js
@@ -1,6 +1,16 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const API = 'http://localhost:8000/restaurants'
+
+export const createRestaurant = createAsyncThunk(
+    'restaurants/createRestaurant',
+    async (newObj) => {
+        let res = await axios.post(API, newObj)
+        return res; 
+    }
+)
+
 const restaurantsSlice = createSlice({
     name: 'restaurants',
     initialState: {
